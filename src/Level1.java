5c22517141589bcb968059d8509cee5578684852
Andrey Otryaskin <kinlydog@gmail.com>
2023-01-05 19:22:19 +1000

    Update project

--- src/Level1.java
+++ src/Level1.java
@@ -2,47 +2,45 @@ public class Level1 {
     public static String BigMinus(String s1, String s2) {
         final int S1_LENGTH = s1.length();
         final int S2_LENGTH = s2.length();
-        final int MAX_LENGTH;
+        final int MAX_LENGTH = Math.max(S1_LENGTH, S2_LENGTH);
 
-        if (S1_LENGTH >= S2_LENGTH) {
-            MAX_LENGTH = S1_LENGTH;
-        } else {
-            MAX_LENGTH = S2_LENGTH;
-        }
-
-        int[] arrayInteger1 = new int[MAX_LENGTH];
-        int[] arrayInteger2 = new int[MAX_LENGTH];
+        int[] number1 = new int[MAX_LENGTH];
+        int[] number2 = new int[MAX_LENGTH];
 
         int[] smallerNumber = new int[MAX_LENGTH];
         int[] biggerNumber = new int[MAX_LENGTH];
 
+        // переводим строки в массив целых чисел
         for (int i = 0; i < S1_LENGTH; i++) {
-            arrayInteger1[i] = s1.charAt(S1_LENGTH - 1 - i) - '0';
+            number1[i] = s1.charAt(S1_LENGTH - 1 - i) - '0';
         }
 
         for (int i = 0; i < S2_LENGTH; i++) {
-            arrayInteger2[i] = s2.charAt(S2_LENGTH - 1 - i) - '0';
+            number2[i] = s2.charAt(S2_LENGTH - 1 - i) - '0';
         }
 
         if (S1_LENGTH > S2_LENGTH) {
-            smallerNumber = arrayInteger2;
-            biggerNumber = arrayInteger1;
-        } else if (S1_LENGTH < S2_LENGTH) {
-            smallerNumber = arrayInteger1;
-            biggerNumber = arrayInteger2;
+            smallerNumber = number2;
+            biggerNumber = number1;
         }
 
+        if (S1_LENGTH < S2_LENGTH) {
+            smallerNumber = number1;
+            biggerNumber = number2;
+        }
+
+        // определяем какое из двух чисел больше
         int x = S1_LENGTH - 1;
         while (S1_LENGTH == S2_LENGTH) {
-            if (arrayInteger1[x] == arrayInteger2[x]) {
+            if (number1[x] == number2[x]) {
                 x--;
-            } else if (arrayInteger1[x] > arrayInteger2[x]) {
-                biggerNumber = arrayInteger1;
-                smallerNumber = arrayInteger2;
+            } else if (number1[x] > number2[x]) {
+                biggerNumber = number1;
+                smallerNumber = number2;
                 break;
             } else {
-                biggerNumber = arrayInteger2;
-                smallerNumber = arrayInteger1;
+                biggerNumber = number2;
+                smallerNumber = number1;
                 break;
             }
 
@@ -51,6 +49,7 @@ public class Level1 {
             }
         }
 
+        // производим вычитание меньшего из большего
         for (int i = 0; i < MAX_LENGTH; i++) {
             if (biggerNumber[i] < smallerNumber[i]) {
                 biggerNumber[i] += 10;
@@ -60,16 +59,18 @@ public class Level1 {
             biggerNumber[i] -= smallerNumber[i];
         }
 
+        // считаем количество нулей в начале числа,
+        // чтобы игнорировать их при конвертации в строку
         int y = 0;
         while (biggerNumber[MAX_LENGTH - 1 - y] == 0) {
             y++;
         }
 
-        StringBuilder bld = new StringBuilder();
+        StringBuilder stringFromNumber = new StringBuilder();
         for (int i = MAX_LENGTH - 1 - y; i >= 0; i--) {
-            bld.append(biggerNumber[i]);
+            stringFromNumber.append(biggerNumber[i]);
         }
 
-        return bld.toString();
+        return stringFromNumber.toString();
     }
 }
